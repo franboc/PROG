15d8adb0d561e8be94512a5b62dab6a7a37949b2
Francesca Bochicchio <fran.bochicchio@gmail.com>
2024-02-22 01:59:52 +0100

    ho cambiato il 5 in 6

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main(int argc, char const *argv[])
 {
     printf("Somma 3+4: %d\n", somma(3, 4));
     printf("Prodotto 4*6: %d\n", prod(4, 6));
-    printf("Quadrato di 5: %d", quad(5));
+    printf("Quadrato di 5: %d", quad(6));
     return 0;
 }   
